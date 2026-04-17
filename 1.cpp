e13af306681811d4752f750f909cae265caefed3
MD. Irfan Uddin <136191786+irfan1537@users.noreply.github.com>
2026-04-17 21:18:55 +0600

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        if (N % 2 == 0)
+        {
+            cout << "even\n";
+        }
+        else
+            cout << "odd\n";
+    }
+}
